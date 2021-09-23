eec589b6e60c3899f1095d7b698426e52cfabaf9
KungPaoChick <matthewriel63@gmail.com>
2021-09-23 20:41:13 +0800

    changed qtile dunst, custom dmenu scripts' colorschemes

--- configs/qtile/scripts/autostart.sh
+++ configs/qtile/scripts/autostart.sh
@@ -4,10 +4,8 @@
 # The need for ksuperkey is only necessary if j4-dmenu-desktop does not show up when the super key is pressed.
 # If you don't have ksuperkey installed, get it from the aur: "yay -S ksuperkey"
 # Enable Super Keys For Menu
-if command -v ksuperkey &> /dev/null; then
-    ksuperkey -e 'Super_L=Alt_L|F1' &
-    ksuperkey -e 'Super_R=Alt_L|F1' &
-fi
+#ksuperkey -e 'Super_L=Alt_L|F1' &
+#ksuperkey -e 'Super_R=Alt_L|F1' &
 
 # set background
 bash $HOME/.config/qtile/scripts/.fehbg
@@ -18,8 +16,8 @@ killall -9 picom sxhkd dunst xfce4-power-manager
 # Launch notification daemon
 dunst \
 -geom "280x50-10+38" -frame_width "1" -font "Source Code Pro Medium 10" \
--lb "#3D3250FF" -lf "#C4C7C5FF" -lfr "#B07190FF" \
--nb "#3D3250FF" -nf "#C4C7C5FF" -nfr "#B07190FF" \
+-lb "#0F131FFF" -lf "#82dbf4FF" -lfr "#548FABFF" \
+-nb "#0F131FFF" -nf "#82dbf4FF" -nfr "#548FABFF" \
 -cb "#2E3440FF" -cf "#BF616AFF" -cfr "#BF616AFF" &
 
 # power manager and picom start
